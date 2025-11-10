a520277764754b7464fa38818d3d1631e09e837a
A1234567-S <70743840+A1234567-S@users.noreply.github.com>
2025-11-10 16:00:03 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,18 @@ import App from './App'
 import EggsPage from './EggsPage'
 import './index.css'
 
+// In main.jsx
+import { lazy, Suspense } from 'react'
+
+const EggsPage = lazy(() => import('./EggsPage'))
+
+// Then wrap routes:
+<Suspense fallback={<div>Loading...</div>}>
+  <Routes>
+    <Route path="/foods/eggs" element={<EggsPage />} />
+  </Routes>
+</Suspense>
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
