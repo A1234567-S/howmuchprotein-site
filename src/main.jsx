3b65d403a29e91ab1ec1b6411cea070e8836faa6
A1234567-S <70743840+A1234567-S@users.noreply.github.com>
2025-11-10 18:59:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
-import EggsPage from './EggsPage'
+import ChickenBreastPage from './ChickenBreastPage'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,8 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/foods/eggs" element={<EggsPage />} />
-        {/* Add more food routes here later */}
+        <Route path="/foods/chicken-breast" element={<ChickenBreastPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
